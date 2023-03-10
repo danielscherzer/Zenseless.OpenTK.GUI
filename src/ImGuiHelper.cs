ca741d36b98dc5479be41e71e7ce5637d67f90d5
Daniel Scherzer <scherzer@rwu.de>
2023-03-10 22:03:41 +0100

    quick commit

--- src/ImGuiHelper.cs
+++ src/ImGuiHelper.cs
@@ -20,6 +20,20 @@ public class ImGuiHelper
 	}
 
 	/// <summary>
+	/// Create a SliderFloat2 with <see cref="Vector2"/> input.
+	/// </summary>
+	/// <param name="label">The label of the slider</param>
+	/// <param name="value">The <see cref="Vector2"/> value to edit.</param>
+	public static void SliderFloat(string label, ref Vector2 value)
+	{
+		System.Numerics.Vector2 sysV = value.ToSystemNumerics();
+		if (ImGui.SliderFloat2(label, ref sysV, -1f, 1f))
+		{
+			value = sysV.ToOpenTK();
+		}
+	}
+
+	/// <summary>
 	/// Create a SliderFloat3 with <see cref="Vector3"/> input.
 	/// </summary>
 	/// <param name="label">The label of the slider</param>
